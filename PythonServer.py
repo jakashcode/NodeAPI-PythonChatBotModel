953b07b1c4e651aa828b89e508991eb2b39365a6
Akash <jkashcode@gmail.com>
2020-05-25 21:44:58 +0530

    feature add

---
+++ PythonServer.py
@@ -0,0 +1,26 @@
+import zerorpc
+import sys
+
+from Model.chatgui import chatbot_response
+from Model.chatgui import *
+
+
+PORT = 4242
+
+class PythonServer(object):
+    def listen(self):
+        print(f'Python Server started listening on {PORT} ...')
+
+    def predict_from_img(self, text):
+        res=chatbot_response(text)
+        return res
+
+try:
+    s = zerorpc.Server(PythonServer())
+    s.bind(f'tcp://0.0.0.0:{PORT}')
+    s.run()
+    print('hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiihiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiihiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiihiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiihiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiihiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiihiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiihiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiihiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii')
+    print('PythonServer running...')
+except Exception as e:
+    print('unable to start PythonServer:', e)
+    raise e
